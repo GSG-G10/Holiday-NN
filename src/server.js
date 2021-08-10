148dd9d46d017b92def78531bf9bdf0ebf294e8f
Nizar7zak <nizar7zakout@gmail.com>
2021-08-10 22:01:48 +0300

    set-up server to be ligth and require important module relates 11

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.set('port', 3000);
 
 const filePath = path.join(__dirname, '..', 'public');
+
+app.use(compression(filePath));
 app.use(express.static(filePath));
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000');
+app.listen(app.get('port'), () => {
+  console.log(`http://localhost:${app.get('port')}`);
 });
