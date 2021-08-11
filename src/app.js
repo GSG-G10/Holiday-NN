093a05000fa9b40db542b6c4fcd971d29afde91e
NourAdawi <nouradwi12@gmail.com>
2021-08-11 16:23:46 +0300

    add error pages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('env2')('.env');
 const express = require('express');
 const compression = require('compression');
+const path = require('path');
 
 const searchRouter = require('./controllers/search');
 const dataRouter = require('./controllers/data');
@@ -15,6 +16,16 @@ app.set('port', port);
 app.use(express.static('public'));
 app.use('/getApi', searchRouter);
 app.use('/getData', dataRouter);
+
+app.use((req, res, next) => {
+  const filePath = path.join(__dirname, '..', 'public', 'Error404.html');
+  res.status(404).sendFile(filePath);
+});
+
+app.use((err, req, res, next) => {
+  const filePath = path.join(__dirname, '..', 'public', 'Error500.html');
+  res.status(500).sendFile(filePath);
+});
 
 app.listen(app.get('port'), () => {
   console.log(`http://localhost:${app.get('port')}`);
