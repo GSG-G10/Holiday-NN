0c676950c45ec21b8c694c6e530da384ba7eaa50
Nizar7zak <nizar7zakout@gmail.com>
2021-08-11 03:23:46 +0300

    create function get options to select tag relates #12

--- public/dom.js
+++ public/dom.js
@@ -1,18 +1,33 @@
-const showDataDev = document.querySelector(".show-data");
+const showDataDev = document.querySelector('.show-data');
+const selectTag = document.querySelector('#country');
 
-const createAndAppend = (elementTag, x, className, value) => {
+const createAndAppend = (elementTag, parentNode, className, textShow) => {
   const nodeDom = document.createElement(elementTag);
   nodeDom.classList.add(className);
-  nodeDom.textContent = value;
-  x.appendChild(nodeDom);
+  nodeDom.textContent = textShow;
+  parentNode.appendChild(nodeDom);
   return nodeDom;
+};
+
+const getOption = (elementTag, parentNode, attribute, valueAtt, textShow) => {
+  const nodeDom = document.createElement(elementTag);
+  nodeDom.setAttribute(attribute, valueAtt);
+  nodeDom.textContent = textShow;
+  parentNode.appendChild(nodeDom);
 };
 
 const subRow = createAndAppend('div', showDataDev, 'sub-row');
 
-const getInformation = (arr) => {
+const getInformationResult = (arr) => {
   arr.forEach((element) => {
     const nameHoliday = createAndAppend('p', subRow, 'name', element.name);
     const dateHoliday = createAndAppend('p', subRow, 'date', element.date.iso);
+  });
+};
+
+const getDataToOptions = (obj) => {
+  const arrResult = Object.keys(obj);
+  arrResult.forEach((ele) => {
+    getOption('option', selectTag, 'value', ele, obj[ele]);
   });
 };
